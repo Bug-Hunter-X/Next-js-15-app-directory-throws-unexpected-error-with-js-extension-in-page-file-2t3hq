a68390e14931ef98c501ea8901cefaea9e59d30c
BugHunter-X <askzenai@gmail.com>
2024-12-15 20:16:06 +0100

    Add bugSolution.jsx file

---
+++ bugSolution.jsx
@@ -0,0 +1,12 @@
+```javascript
+// app/page.jsx
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <p>This is some text.</p>
+    </div>
+  );
+}
+```
+Rename `page.js` to `page.jsx` to resolve the issue.  This simple change fixes the unexpected error.
